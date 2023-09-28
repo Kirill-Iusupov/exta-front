ca4385b780ac6d7ed780d6b1eba179bca75bcda1
AprioriDev <apriorifox.dev@gmail.com>
2023-09-28 14:12:23 +0600

    v1 complete

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,9 +8,9 @@ const Header = () => {
     return (
         <div>
             <Wrapper >
-                <div className='border-[5px] border-t-[0] border-solid border-[#C23594]  rounded-b-[25px] flex items-center justify-between p-[15px] bg-[black]'>
+                <div className='border-[5px] border-t-[0] border-solid border-[#C23594] rounded-b-[25px] flex items-center justify-around p-[15px] bg-[black]'>
                     <Logo />
-                    <h2 className='text-3xl font-serif dark:text-white'>"Для моментов страсти и близости"</h2>
+                    <h2 className='text-3xl font-serif dark:text-white w-[750px] text-center'>"Для моментов страсти и близости"</h2>
                     <Contacts />
                 </div>
             </Wrapper>
