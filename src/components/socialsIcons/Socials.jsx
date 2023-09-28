7b4e5efacb9615b60cedf0d14ad99c0e9d520a0f
AprioriDev <apriorifox.dev@gmail.com>
2023-09-28 13:34:04 +0600

    made some lil changes

--- src/components/socialsIcons/Socials.jsx
+++ src/components/socialsIcons/Socials.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import {FaWhatsapp, FaTelegramPlane, FaInstagram} from "react-icons/fa"
+
+
 const Socials = () => {
     return (
         <div className='w-[150px] h-[50px] flex justify-between items-center m-auto'>
-            <a href="https://wa.me/996558690069" target={"_blank"}>
+            <a href="https://wa.me/996558690069" target={"_blank"} >
                 <FaWhatsapp size={"30px"} />
             </a>
-            <a href="https://t.me/extaseaKG" target={"_blank"}>
+            <a href="https://t.me/extaseaKG" target={"_blank"} >
                 <FaTelegramPlane size={"30px"} />
             </a>
-            <a href="https://www.instagram.com/extasea_kg/" target={"_blank"}>
+            <a href="https://www.instagram.com/extasea_kg/" target={"_blank"} >
                 <FaInstagram size={"30px"} />
             </a>
         </div>
